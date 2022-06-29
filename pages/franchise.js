81a04704edf65cd33baea7cb6114632e03a93aea
yaddyvirus <yadullahabidi@gmail.com>
2022-06-30 03:53:08 +0530

    Franchise page overhaul

--- pages/franchise.js
+++ pages/franchise.js
@@ -1,12 +1,165 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import styles from "../styles/Franchise.module.css"
 
 export default function Franchise() {
     return (
         <div>
             <Header></Header>
-            {/* Franchise page */}
-            <div className="container col-xl-10 col-xxl-8 px-4 py-5" id='contact'>
+            {/* Franchise page v2 */}
+            <div className="container-fluid h-100 bg-black text-light text-end fw-bold text-uppercase row">
+                <div className="col-md-6">
+                    <img src="/franch-1.png" className="img-fluid" alt="" />
+                </div>
+                <div className="col-md-6 d-flex flex-column justify-content-center">
+                    <h1 className={styles.title}>Own a franchise</h1>
+                    <h1 className={`${styles.golden} text-end`}>Be your own boss</h1>
+                </div>
+            </div>
+            <div className="container-fluid row my-5 px-5 ">
+                <div className="col-md-6">
+                    <h1 className={`${styles.someDiv} display-1 fw-bold text-uppercase`}>If you are<br></br> looking to</h1>
+                    <ul className="display-6">
+                        <li>Own a salon business</li>
+                        <li>To have something of your own</li>
+                        <li>And most importantly, want to join our family</li>
+                    </ul>
+                </div>
+                <div className="col-md-6 d-flex flex-column justify-content-center align-items-center">
+                    <img src="/franch-2.png" className="img-fluid" alt="" />
+                </div>
+                <div className="text-center my-3">
+                    <button className="btn btn-outline-warning btn-lg">Join us</button>
+                </div>
+            </div>
+
+            {/* Franchise models div */}
+            <div className="contianer-fluid ">
+                <div className={`${styles.shadowBox} container p-0`}>
+                    <h3 className="p-3">We're one of the few brands that allows partners to leave day to day operations to the brand and enjoy themselves</h3>
+
+                    {/* Franchise Models */}
+                    {/* Model A */}
+                    <div className={`${styles.modelTitle} bg-dark`}>
+                        <p className="lead px-5 py-3 fw-bold">Franchise Model A</p>
+                    </div>
+                    <div className="container p-5">
+                        <div className="row">
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-1.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Mininum Area Required</p>
+                                <p>700 sq ft</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-2.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Total Investment</p>
+                                <p>INR 22-30 Lacs</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-3.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Franchise Fee</p>
+                                <p>INR 6 Lacs</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-4.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Average Payback</p>
+                                <p>20-30 Months</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-5.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Mode of Operations</p>
+                                <p>FOCO/FOFO</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-6.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Agreement Term</p>
+                                <p>5 years</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Model B */}
+                    <div className={`${styles.modelTitle} bg-dark `}>
+                        <p className="lead px-5 py-3 fw-bold">Franchise Model B</p>
+                    </div>
+                    <div className="container p-5">
+                        <div className="row">
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-1.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Mininum Area Required</p>
+                                <p>700 sq ft</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-2.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Total Investment</p>
+                                <p>INR 22-30 Lacs</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-3.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Franchise Fee</p>
+                                <p>INR 6 Lacs</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-4.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Average Payback</p>
+                                <p>20-30 Months</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-5.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Mode of Operations</p>
+                                <p>FOCO/FOFO</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-6.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Agreement Term</p>
+                                <p>5 years</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Model C */}
+                    <div className={`${styles.modelTitle} bg-dark`}>
+                        <p className="lead px-5 py-3 fw-bold">Franchise Model C</p>
+                    </div>
+                    <div className="container p-5">
+                        <div className="row">
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-1.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Mininum Area Required</p>
+                                <p>700 sq ft</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-2.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Total Investment</p>
+                                <p>INR 22-30 Lacs</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-3.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Franchise Fee</p>
+                                <p>INR 6 Lacs</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-4.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Average Payback</p>
+                                <p>20-30 Months</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-5.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Mode of Operations</p>
+                                <p>FOCO/FOFO</p>
+                            </div>
+                            <div className="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                <img src="/franch-icon-6.png" className="img-fluid" alt="" />
+                                <p className="fw-bold mt-2">Agreement Term</p>
+                                <p>5 years</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Franchise page form*/}
+            <div className="container col-xl-10 col-xxl-8  py-5" id='contact'>
                 <div className="row align-items-center g-lg-5 py-5">
                     <div className="col-lg-7 text-center text-lg-start">
                         <h1 className="display-4 fw-bold lh-1 mb-3">Work with us</h1>
@@ -35,9 +188,6 @@ export default function Franchise() {
                             <div className="form-floating mb-3">
                                 <select className="form-select" id="floatingSelect" aria-label="size">
                                     <option selected>700 sq ft</option>
-                                    {/* <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option> */}
                                 </select>
                                 <label htmlFor="floatingSelect">Size</label>
                             </div>
@@ -48,14 +198,14 @@ export default function Franchise() {
                                 <label htmlFor="floatingSelect">Investment</label>
                             </div>
                             <div className="form-floating mb-3">
-                                <input type="text" className="form-control" name="cityName" placeholder="cityName"/>
+                                <input type="text" className="form-control" name="cityName" placeholder="cityName" />
                                 <label htmlFor="">City of Interest</label>
                             </div>
                             <div className="form-floating mb-3">
                                 <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: "100px" }}></textarea>
                                 <label htmlFor="floatingTextarea2">Comments/Message</label>
                             </div>
-                            <button className="w-100 btn btn-lg btn-primary" type="submit">Get in touch</button>
+                            <button className="w-100 btn btn-lg btn-warning" type="submit">Get in touch</button>
                         </form>
                     </div>
                 </div>
